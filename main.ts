934cef2f087d66936aa8dad3acf465e264e575a8
Satya Rohith <me@satyarohith.com>
2021-02-22 23:52:23 +0530

    groundbreaking update

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { serve, json } from "https://deno.land/x/sift@0.1.4/mod.ts";
 
 serve({
-  "/": () => json({ message: "Hello, world!" })
+  "/": request => {
+    console.log(request);
+    return json({ message: "Hello, world!" });
+  }
 });
